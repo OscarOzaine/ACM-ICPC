ce21d38b109864cf5e02de5747ee31f49d81d590
Oscar Ozaine <oscar.ozaine@gmail.com>
2017-04-25 22:35:35 -0700

    cleaaning
    still having issues with some test cases

--- World Finals/2015/keyboarding.py
+++ World Finals/2015/keyboarding.py
@@ -4,145 +4,69 @@ import sys
 from collections import deque
 
 class Node:
-    def __init__(self, point, parent=None):
+    def __init__(self, point, parent=None, position=None):
         self.point = point
         self.parent = parent
-
-def getNeighbors(matrix, x, y):
-    neighbors = []
-
-    if y >= 0 and y < len(matrix[x]) - 1:
-        node = Node(matrix[x][y+1], matrix[x][y])
-        neighbors.append(node)
+        self.position = position
 
 
-    if y >= 1 and y < len(matrix[x]):
-        node = Node(matrix[x][y-1], matrix[x][y])
-        neighbors.append(node)
-        #neighbors.append(matrix[x][y-1])
-    
-    if x >= 1 and x < len(matrix):
-        #node = Node(matrix[x][y+1], matrix[x][y])
-        #neighbors.append(matrix[x+1][y])
-        node = Node(matrix[x+1][y], matrix[x][y])
-        neighbors.append(node)
-
-    if x >= 2 and x <= len(matrix):
-        #node = Node(matrix[x][y+1], matrix[x][y])
-        #neighbors.append(matrix[x-1][y])
-
-        node = Node(matrix[x-1][y], matrix[x][y])
-        neighbors.append(node)
-    
-    return neighbors
-
-
-def bfs(node,goal,size,grid):
+def bfs(node, start, goal, grid, x, y):
     queue = deque()
     queue.append(node)
-    explored.append(node.point)
+    explored = []
+    explored.append(node.position)
     while queue:
-        t = queue.popleft()
-        if t.point not in explored:
-            explored.append(t.point)
-        if t.point == goal:
-            return t
-        for child in children(t.point,size,grid):
-            if child not in explored:
-                u = Node(child,t)
-                queue.append(u)
-    return None
-# Tail starts here
-'''
-def bfs(graph, start, end):
-    print 'bfs'
-    visited, queue = set(), [start]
-    counter = 0
-    nodes = []
-    while queue:
-        #print queue
-        #for i in queue:
-            #print i,
-        node = queue.pop(0)
-        
-        counter += 1
-        print node.point, start.point, end.point
-        if node.point == end.point:
-            
+        node = queue.popleft()
+        if node.position not in explored:
+            explored.append(node.position)
+
+        if node.point == goal:
             return node
 
-        elif node.point not in visited and node.point not in queue:
-            visited.add(node.point)
-
-            for n in graph[node.point]:
-                if node.point != '*':
-                    if n.point not in visited:
-                        queue.append(n)
-            
-
+        for x, y in children(grid, node.position[0], node.position[1]):
+            if (x,y) not in explored:
+                u = Node(grid[x][y], node, (x, y))
+                queue.append(u)
     return None
-'''
-def children(point,size,grid):
-    print point
-    x,y = point
-    size_x, size_y = size
+
+def children(matrix, x, y):
+    size_x = len(matrix)
+    size_y = len(matrix[1])
     children = [(x-1, y),(x,y - 1),(x,y + 1),(x+1,y)]
-    return [child for child in children if grid[child[0]][child[1]] != '%']
+    return [child for child in children if (child[0] >= 0 and child[1] >= 0) and (child[0] < size_x and child[1] < size_y)]
 
-def createGraph(matrix):
-    graph = {}
-    enter = []
-    nodes = {}
-    for i in range(1, len(matrix) + 1):
-        for j in range(0, len(matrix[i])):
-            print matrix[i][j]
-            node = Node(matrix[i][j], None)
-            graph[node.point] = None
-            if matrix[i][j] == '*':
-                if graph[node.point] != None:
-                    graph[node.point] = graph[node.point] + getNeighbors(matrix, i, j)
-                else:
-                    graph[node.point] = getNeighbors(matrix, i, j)
-            else:
-                graph[node.point] = getNeighbors(matrix, i, j)
+def getPath(node):
+    path = []
+    while node != None:
+        path.append(node)
+        node = node.parent
+    return path
 
-    return graph
-explored = []
 def keyboarding(rows, columns, matrix, word):
-    graph = createGraph(matrix)
-    print graph
-
-    print matrix
     pos = 0
-    last_v = Node(matrix[1][0])
-    while pos < len(word) - 1:
-        #def bfs(node,goal,size,grid):
-        #node = Node((pacman_x, pacman_y))
-        node = bfs(last_v, Node(word[pos]),(rows,columns),matrix)
+    start = Node(matrix[0][0], None, (0, 0))
+    counter = 0
+    while pos < len(word):
 
-        #node = bfs(graph, last_v, Node(word[pos], None))
+        node = bfs(start, start, word[pos], matrix, start.position[0], start.position[1])
         if node != None:
-            
-            path = []
-            while node != None:
-                print node
-                path.append(node.point)
-                node = node.parent
-            path.reverse()
-            
-        last_v = Node(word[pos])
-        print
+            path = getPath(node)
+            counter += len(path)
+            start = Node(word[pos], None, path[0].position)
         pos+=1
 
-    print 'stuif'
-    bfs(graph, last_v, Node('*', None))
+    start = Node(node.point, None, path[0].position)
+    node = bfs(start, start, '*', matrix, start.position[0], start.position[1])
+    path = getPath(node)
+    counter += len(path)
+    return counter
 	
 matrix = {}
 while True:
     #try:
 
     rows, columns = map(int, raw_input().strip().split(' '))
-    for i in range(1, rows + 1):
+    for i in range(0, rows):
         matrix[i] = list(raw_input().strip())
         #print matrix[i]
     word = list(raw_input().strip())
